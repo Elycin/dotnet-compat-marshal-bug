ca20b2f3c8494e45406eab593d9aa4271cc66f86
Ely Haughie <elycin2@gmail.com>
2018-08-15 14:21:36 -0400

    Adding comments

--- dotnet-compat-marshal-bug/Program.cs
+++ dotnet-compat-marshal-bug/Program.cs
@@ -8,23 +8,33 @@ namespace dotnet_compat_marshal_bug
     {
         static void Main(string[] args)
         {
+            // Initialize the wrapper library.
             var symLinkLib = new Symlink();
 
-            if (symLinkLib.Environment.Create("./symlink_example", "C:/Users/"))
+            // Here I'm using an example path.
+            if (symLinkLib.Environment.Create("C:/ExampleSymlinkPath", "C:/Users/Public"))
             {
+                // Success.
                 Console.WriteLine("The symlink was successful");
                 Environment.Exit(0);
             }
             else
             {
+                // Get the error code
                 var errorVal = Marshal.GetLastWin32Error();
 
+                // Failure.
                 Console.WriteLine("The symlink has failed.");
                 Console.WriteLine("Marshal Error: {0} ({1})", errorVal, MarshalDecoder(errorVal));
                 Environment.Exit(1);
             }
         }
 
+        /// <summary>
+        /// Example function to return string from the error code.
+        /// </summary>
+        /// <param name="marshal"></param>
+        /// <returns></returns>
         static string MarshalDecoder(int marshal)
         {
             switch (marshal)
